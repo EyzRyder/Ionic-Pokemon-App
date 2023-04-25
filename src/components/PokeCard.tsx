1b2f52f608b4b5d87d02ece618c3c8cb38ec752c
EyzRyder <85580011+EyzRyder@users.noreply.github.com>
2023-04-25 19:40:11 -0300

    2 files modified

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -9,11 +9,12 @@ interface PokeCardProps {
 
 const PokeCard: React.FC<PokeCardProps> = (props) => {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 px-3 py-2 bg-slate-600 w-48 "> <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${getIdFromUrl(props.url)}.png `} alt={props.name} />
+    <div className="flex flex-col justify-center items-center gap-2 px-10 py-8 bg-slate-600 w-48 rounded-lg ">
+      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${getIdFromUrl(props.url)}.png `} alt={props.name} />
       <h2 className='text-primary-default font-semibold'>
         {props.name}
+        <span> #{getIdFromUrl(props.url)}</span>
       </h2>
-      <p>{getIdFromUrl(props.url)}</p>
       <Link to={`pokemon/${getIdFromUrl(props.url)}`} className='bg-tertiary-default py-2 px-3 rounded-md'> Ver </Link>
     </div>
   );
--- src/pages/PokemonDetails.tsx
+++ src/pages/PokemonDetails.tsx
@@ -5,7 +5,6 @@ import {
 import './Home.scss';
 import { useQuery } from 'react-query';
 import { getOnePokemon } from '../services/pokeApi';
-import PokeCard from '../components/PokeCard';
 import { Pokemon } from '../types/pokemon';
 import { useParams } from 'react-router';
 
@@ -16,18 +15,55 @@ const PokemonDetails: React.FC = () => {
     return response.data
   });
 
+  const typeColors = {
+    "background":"red"
+  }
+
   return (
     <IonPage>
       <IonContent fullscreen>
-        {isFetching && (
-          <div> loading</div>
-        )}
-        {data && <div className="flex flex-col justify-center items-center gap-2 px-3 py-2 bg-slate-600 w-48 "> <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${data.id}.png `} alt={data.name} />
-          <h2 className='text-primary-default font-semibold'>
-            {data.name}
-          </h2>
-          <p>{data.id}</p>
-        </div>}
+        <div className='flex justify-center items-center min-h-screen relative'>
+
+          {isFetching && (
+            <div className='absolute'> loading</div>
+          )}
+          {data &&
+            <div className="flex flex-col justify-center items-center gap-2 px-10 py-8 bg-slate-600 rounded-lg">
+              <h2 className='text-primary-default font-semibold'>
+                {data.name}
+                <span>#{data.id}</span>
+              </h2>
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${data.id}.png `} alt={data.name} />
+              <table className='flex flex-col gap-2'>
+                <tr className='flex flex-row gap-2'>
+                  <td className='font-bold'>Height</td>
+                  <td>{data.height} cm</td>
+                </tr>
+                <tr className='flex flex-row gap-2'>
+                  <td className='font-bold'>Weight</td>
+                  <td>{data.weight} kg</td>
+                </tr>
+              </table>
+              <div>
+                <h3>Types</h3>
+                <div className='flex  gap-2'>
+                  {data.types.map(type => (
+                    <span className='px-3 py-1 rounded-full' style={typeColors}>
+                      {type.type.name}
+                    </span>
+                  ))}
+                </div>
+                <h3>Abilities</h3>
+                <div className='flex gap-2'>
+                  {data.abilities.map(abilityData => (
+                    <span>
+                      {abilityData.ability.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>}
+        </div>
       </IonContent>
     </IonPage>
   );
